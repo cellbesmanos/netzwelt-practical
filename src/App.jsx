3c69fb631f0cbbf7da730679a0e86af36ecfeaab
cellbesmanos <cellbesmanos0119@gmail.com>
2023-02-28 17:38:20 +0800

    Feat: Set up initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
